87581529cadfe59f098e6de57289ecb98f320a16
Yacine667 <sid.yacinee@gmail.com>
2022-10-10 09:32:37 +0200

    ajout count panier

--- recap.php
+++ recap.php
@@ -11,9 +11,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recapitulatif des produits</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Gemunu+Libre&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Gemunu+Libre&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
@@ -29,6 +30,19 @@
                 <nav class="navbar navbar-expand-md navbar-light ">
                 <a class="navbar-brand" href="http://localhost/appli/index.php">Ajout De Produit |</a>
                 <a class="navbar-brand" href="http://localhost/appli/recap.php">|  Récapitulatif Des Produits</a>
+                <a class="navbar-brand" href="http://localhost/appli/recap.php"><i class="fa-solid fa-basket-shopping"></i></a>
+                <?php 
+
+                    if (empty ($_SESSION['products'])){
+                        echo "Vide";
+                    }
+
+                    else {
+                        echo count($_SESSION['products']);
+                    };
+                
+                
+                ?>
                 </nav>
     
 
